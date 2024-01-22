65a7180a2fb4d220291762612cf79f36113acb8f
Makima3 <natalia.kolchuk@gmail.com>
2024-01-22 23:30:59 +0200

    Lesson 4.1

--- src/Components/PostContain/Posts.js
+++ src/Components/PostContain/Posts.js
@@ -1,14 +1,13 @@
 import {useEffect, useState} from "react";
-import {post} from "axios";
 import {postService} from "../../Services/postService";
 import {Post} from "./Post";
 
 const Posts = () => {
 
-    const [posts, setPosts]=useState([])
+    const [post, setPost]=useState([])
 
     useEffect(()=>{
-        postService.getByPostId().then(({data})=> setPosts(data))
+        postService.getByPostId().then(({data})=> setPost(data))
     }, [])
 
 
